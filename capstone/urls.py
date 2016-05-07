8256327b77b4953c863595586dffb4e18b24a1b9
jchu2 <jchu2@andrew.cmu.edu>
2016-05-06 20:38:06 -0400

    Update urls.py

--- capstone/urls.py
+++ capstone/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 from capstone import views as capstone_view
 urlpatterns = [
     url(r'^$',capstone_view.home,name='home'),
-    url(r'^upload', capstone_view.upload, name='upload'),
+    url(r'^upload crash',capstone_view.uploadCrash,name='upload crash'),
+    url(r'^upload vehicle',capstone_view.uploadVehicle,name='upload vehicle'),
+    url(r'^upload person',capstone_view.uploadPerson,name='upload person'),
+    url(r'^crash', capstone_view.crash_load,name = 'crash'),
+    url(r'^vehicle', capstone_view.uploadVehicle,name = 'vehicle'),
+    url(r'^person', capstone_view.uploadPerson,name='person'),
     url(r'^api/getMajorOrFatal',capstone_view.getMajorFatal),
     url(r'^api/getCrashByCollisionType',capstone_view.getCrashByCollisionType),
     url(r'^api/getCrashByIntersectionType',capstone_view.getCrashByIntersectionType),
