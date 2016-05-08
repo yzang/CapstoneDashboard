396ec9a6eb957b2999f62d491b08d4191aa2a66d
jchu2 <jchu2@andrew.cmu.edu>
2016-05-07 23:12:30 -0400

    Update import_vehicle_csv.py

--- data_processing/import_vehicle_csv.py
+++ data_processing/import_vehicle_csv.py
@@ -10,7 +10,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'CapstoneDashboard.settings'
 
 def import_vehicle_csv(file):
     application = get_wsgi_application()
-    from capstone.models import vehicle
+    from capstone.models import Vehicle
     file_name = os.path.join("data_processing/vehicle/", file)
     with open(file_name, "rb") as infile:
         reader = csv.reader(infile)
