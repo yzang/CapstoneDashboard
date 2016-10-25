14634e34a7a38556260a1359888c5427ddf66f91
timwis <tim@timwis.com>
2016-10-25 11:43:16 -0400

    Secret and Debug settings should use env vars. Closes #4

--- CapstoneDashboard/settings.py
+++ CapstoneDashboard/settings.py
@@ -21,10 +21,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^m_tf%_*%heqjp9v&-!m_#)dlwfqcwm3&sl)2u4ll)jayw=9)h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '^m_tf%_*%heqjp9v&-!m_#)dlwfqcwm3&sl)2u4ll)jayw=9)h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') == 'True')
 
 
 # Application definition
